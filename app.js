608fb34e6a0f8d995cbc6db558613a51c8cef7ad
Hari Sachdeva <hsachdevaucd@gmail.com>
2020-05-10 12:37:49 -0700

    Update app.js for Postman

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.get("/", (req, res) => {
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
-app.use("/api/users", users);
-app.use("/api/tweets", tweets);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
